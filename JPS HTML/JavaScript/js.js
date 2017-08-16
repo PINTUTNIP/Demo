7a62b01395ce6de8234d6028cbab96ec65b89a52
PINTUTNIP <jyotiprakashsahu27@gmail.com>
2017-08-16 15:20:49 +0530

    My Webpage

---
+++ JPS HTML/JavaScript/js.js
@@ -0,0 +1,9 @@
+/*alert("Welcome");
+var X=prompt("Enter X");
+var Y=prompt("Enter Y");
+var Z=parseInt(X)+parseInt(Y);
+alert(Z);*/
+function changeText(id)
+{
+	document.getElementById(id).innerHTML="College Of Engineering & Technology";
+}
